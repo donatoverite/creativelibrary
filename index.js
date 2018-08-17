3bf98a8c46585e890c3d322db1cd3599d271c18f
aeraibrooks <aeraibrooks@gmail.com>
2018-08-17 10:46:04 -0400

    update

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.set('view engine', 'ejs');
 app.get('/', function(req, res) {
 
     // ejs render automatically looks in the views folder
-    res.render('index.html');
+    res.render('index');
 });
 
 app.listen(port, function() {
