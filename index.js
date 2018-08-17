5d953858d2763adb086f3d06d00f4c4310d089f1
aeraibrooks <aeraibrooks@gmail.com>
2018-08-17 10:13:41 -0400

    update

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var app = require('express')();
 var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var io = require('socket.io')(https);
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
-http.listen(4000, function(){
+https.listen(4000, function(){
   console.log('listening on *:4000');
 });
